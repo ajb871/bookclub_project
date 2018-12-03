f724a4198077b38578be989a49534f9252b70ce9
ajb871 <ajb871@nyu.edu>
2018-12-03 15:12:35 -0500

    added css styling to pages

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+// Dependencies //
 var express = require('express')
 var app = express()
 var ejs = require('ejs');
 const fs = require('fs');
-
 var request = require('request');
 var bodyParser = require('body-parser');
+
+// Views & Static
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('./'));
 app.set('view engine', 'ejs');
+
+// Load data from users data JSON file
 let userFile = fs.readFileSync('users.json');
 let userData = JSON.parse(userFile);
 
 
-// sign up page here
+////// Sign up Page here //////
 app.get('/', function(req, res){
   res.render('signup', {success: null, error: null});
 });
@@ -21,34 +25,40 @@ app.post('/', function(req, res){
   let username = req.body.username;
   let password = req.body.password;
   let password2 = req.body.password2;
-  let confirmUser = false;
+  let confirmUsername = false;
 
+  // Check if username is taken & send back error if true
   for (let i=0; i<userData.length; i++){
     if (userData[i].username === username){
-      confirmUser = true;  
+      confirmUsername = true;  
     }
   }
 
-  if (confirmUser){
-    res.render('signup', {success: null, error: 'Username already exists, try again!'});
-  }else{
+  if (confirmUsername){
+    res.render('signup', {success: null, error: 'That username is taken, try again!'});
+  } else {
     if (password === password2){
+      // If free username & passwords match, push & write to userdata
       userData.push({username: username, password: password});
       let finalData = JSON.stringify(userData);
       fs.writeFile('users.json', finalData, finished);
+
       function finished(err) {
         console.log(req.body);
       }
-      res.render('signup', {success: "Welcome, new user!"});
-    }else{
+
+      // This will redirect to user's Home page
+      res.render('signup', {success: "Welcome, new user!", error: null});
+
+    } else {
       res.render('signup', {success: null, error: "Passwords have to be the same!"});
       console.log(password, password2);
-
     }   
   }
-});
+}
+);
 
-// log in page here
+////// Log in Page here //////
 app.get('/login', function(req, res){
   res.render('login', {error: null});
 });
@@ -60,7 +70,7 @@ app.post('/login', function(req, res){
   let gotUser = false;
   let userIndex = null;
 
-  ///check for user and password
+  ///Check for username and password
   for (let i=0; i<userData.length; i++){
     if (userData[i].username === username){
       gotUser = true;
@@ -92,6 +102,7 @@ app.post('/login', function(req, res){
 // app.post('/clubBookName', function (req, res){
 
 // })
+
 
 app.listen(3000, function(){
   console.log('app listening on port 3000!')
